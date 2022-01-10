284b2abc2a2f319c03076de9edd7a619d95e9c9c
hyunkim <han.kim@kakao.com>
2022-01-11 08:31:37 +0900

    [chore] 컨트롤러 배치 수정

--- src/main/java/com/web/controller/HomeController.java
+++ src/main/java/com/web/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.web.controller;
 
+import com.domain.Account.Account;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -14,17 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HomeController {
 
-
     @GetMapping(value="/")
     public String home() throws Exception {
         return "home";
-    }
-
-
-    @GetMapping("/login")
-    public String login(){
-
-        return "login/logIn";
     }
 
 }
--- src/main/java/com/web/controller/login/LoginController.java
+++ src/main/java/com/web/controller/login/LoginController.java
@@ -1,39 +1,21 @@
-package com.web.controller.Account;
-
+package com.web.controller.login;
 
 import com.domain.Account.Account;
-import com.web.dto.CompanyRegisterVO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class AccountController {
+public class LoginController {
 
-    @GetMapping( "/register")
-    public String register() {
-        return "register/main";
+    @GetMapping("/login")
+    public String login(){
+
+        return "login/logIn";
     }
-
-    @GetMapping("/register/company")
-    public ModelAndView createCompany(){
-        return new ModelAndView("register/company").addObject(new CompanyRegisterVO());
-    }
-
-    @PostMapping("/register/company")
-    public String createCompany(@ModelAttribute CompanyRegisterVO companyRegisterVO){
-        // TODO : 성공, 실패 분기 必
-
-        return "register/company";
-    }
-
 
     @GetMapping("/denied")
     public String accessDenied(@RequestParam(value = "exception", required = false) String exception, Model model){
@@ -45,5 +27,6 @@ public class AccountController {
 
         return "/login/denied";
     }
+
 
 }
