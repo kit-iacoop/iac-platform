7c6ae46ef4c39649557e310ffee934c59614b7a2
박유경 <51533341+codename-602@users.noreply.github.com>
2022-01-10 15:03:56 +0900

    [chore] home 기능 관련 url 설계

--- src/main/java/com/web/controller/HelloController.java
+++ src/main/java/com/web/controller/HelloController.java
@@ -1,16 +1,31 @@
 package com.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "hello world";
+    @RequestMapping(value = "/")
+    public String home() {
+        return "home";
     }
 
+    @RequestMapping(value = "/login.do")
+    public String login() {
+        return "login/login";
+    }
+
+    @RequestMapping(value = "/register")
+    public String signup() {
+        return "signup/main";
+    }
+
+    @RequestMapping(value = "/register/company")
+    public String signupCompany() {
+        return "signup/company";
+    }
 }
