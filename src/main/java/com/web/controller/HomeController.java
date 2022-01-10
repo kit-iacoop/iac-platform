bc2ea1ad4e0deb996899483f972dbe41b8ac26d1
hyunkim <han.kim@kakao.com>
2022-01-10 19:56:38 +0900

    [chore] (Hello -> Home) Controller

---
+++ src/main/java/com/web/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping(value="/")
+    public String home() throws Exception {
+        return "home";
+    }
+
+}
