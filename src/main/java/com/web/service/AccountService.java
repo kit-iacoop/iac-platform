e0bce13641f25831e299daf266c909f5c15bfa2f
hyunkim <han.kim@kakao.com>
2022-01-11 11:28:27 +0900

    [add] createAccount()

---
+++ src/main/java/com/web/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.web.service;
+
+import com.domain.account.Account;
+
+public interface AccountService {
+    public void createAccount(Account account);
+
+}
+
--- src/main/java/com/web/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/web/service/Impl/AccountServiceImpl.java
@@ -1,6 +1,23 @@
 package com.web.service.Impl;
 
-public class AccountServiceImpl {
+import com.domain.account.Account;
+import com.domain.account.AccountRepository;
+import com.web.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 
+@AllArgsConstructor
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    @Override
+    public void createAccount(Account account) {
+        accountRepository.save(account);
+    }
 }
