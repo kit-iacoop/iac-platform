169aaf9fb7dbe97b7b4347bcf973bd23d0fb0637
lichee55 <lichee55@gmail.com>
2022-01-11 16:22:35 +0900

    [chore] 리베이스 결과 pull하며 변경된 부분 적용

--- src/main/java/com/domain/ApplicationRegistration/ApplicationRegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.domain.ApplicationRegistration;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRegistrationRepository extends JpaRepository<ApplicationRegistration, Long> {
-
-}
--- src/main/java/com/domain/ParticipantCopyright/ParticipantCopyrightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.domain.ParticipantCopyright;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipantCopyrightRepository extends JpaRepository<ParticipantCopyright, Long> {
-    
-}
