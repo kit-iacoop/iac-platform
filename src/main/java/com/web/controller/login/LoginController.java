2e52e3deb7b6a8f800c3dbebe8f591dd6b0d6d18
hyunkim <han.kim@kakao.com>
2022-01-11 14:32:14 +0900

    [add] 로그아웃

--- src/main/java/com/web/controller/login/LoginController.java
+++ src/main/java/com/web/controller/login/LoginController.java
@@ -3,10 +3,14 @@ package com.web.controller.login;
 import com.domain.account.Account;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -22,7 +26,7 @@ public class LoginController {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if(auth.getPrincipal() != null){
+        if(auth.getPrincipal() != null){ // 로그인 체크
             Account principal = (Account)auth.getPrincipal();
             model.addAttribute("username", principal.getName());
         }
@@ -32,5 +36,16 @@ public class LoginController {
         return "/login/denied";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest req, HttpServletResponse res){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(req, res, authentication);
+        }
+
+        return "redirect:/login";
+    }
 
 }
