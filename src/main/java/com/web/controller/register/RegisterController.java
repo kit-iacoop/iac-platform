55194b60b11764a49f24361567d079605c059ba6
hyunkim <han.kim@kakao.com>
2022-01-11 11:46:07 +0900

    [add] 회사 회원가입

--- src/main/java/com/web/controller/register/RegisterController.java
+++ src/main/java/com/web/controller/register/RegisterController.java
@@ -1,7 +1,13 @@
 package com.web.controller.register;
 
 
-import com.web.dto.CompanyRegisterVO;
+import com.domain.account.Company;
+import com.domain.common.State;
+import com.web.dto.CompanyRegisterDTO;
+import com.web.service.AccountService;
+import com.web.service.Impl.AccountServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +15,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@AllArgsConstructor
+
 @Controller
 public class RegisterController {
+
+    private final AccountService accountService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping( "/register")
     public String register() {
@@ -19,17 +30,29 @@ public class RegisterController {
 
     @GetMapping("/register/company")
     public ModelAndView createCompany(){
-        return new ModelAndView("register/company").addObject(new CompanyRegisterVO());
+        return new ModelAndView("register/company").addObject(new CompanyRegisterDTO());
     }
 
     @PostMapping("/register/company")
-    public String createCompany(@ModelAttribute CompanyRegisterVO companyRegisterVO){
-        // TODO : 성공, 실패 분기 必
+    public String createCompany(@ModelAttribute CompanyRegisterDTO companyDTO){
 
-        return "register/company";
+        try {
+            companyDTO.setPassword(passwordEncoder.encode(companyDTO.getPassword())); // 'raw password' to 'bcrypt hash value'
+            Company companyEntity = companyDTO.toEntity();
+            companyEntity.verification("BRONZE", 0L, 0L, State.NORMAL); //TODO : 가입 승인 페이지 생길 때 까지..
+            accountService.createAccount(companyEntity);
+
+        } catch(NumberFormatException NFE){
+            // FE에서 입력 값 예외처리 해줄 것으로 생각.
+            // -> 정상적인 접근이라면 이곳으로 오면 안된다.
+
+            return "register/company"; // 임시 코드
+            //FE 예외처리 코드 생기면 아래로 return 코드 대체
+            //return "redirect:/denied?exception=invalid request
+        }
+        //TODO : 회원가입 완료 페이지 필요
+        return "redirect:/";
     }
-
-
 
 
 }
