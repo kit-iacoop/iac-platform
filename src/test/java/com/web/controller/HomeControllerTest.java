2f42e31f6e7c0fbc5f3f36b85f6f924d7efda55b
hyunkim <han.kim@kakao.com>
2022-01-13 12:53:09 +0900

    [chore] 주석 수정

--- src/test/java/com/web/controller/HomeControllerTest.java
+++ src/test/java/com/web/controller/HomeControllerTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -14,12 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HomeController.class)
-@WebAppConfiguration
-@AutoConfigureMockMvc
-@WithMockUser //TODO : mock 이용 테스트에 SpringSecurity 적용
-//https://imbf.github.io/spring/2020/08/18/Test-Spring-MVC-Controller-Applying-Spring-Security.html
+@ExtendWith(SpringExtension.class) //  Spring Bean 테스트하기 위해서 필요
+@ContextConfiguration(classes = {HomeController.class}) // 테스트를 위해 ApplicationContext 설정, 로드 방법 정의 사용되는 메타데이터 정의
+@WebAppConfiguration    //web과 연관된 구성요소들(controller, view resolver, …)등 다루는 기능 제공
+@AutoConfigureMockMvc   //자동으로 MockMvc를 사용해서 테스트할 수 있게끔 설정
+@WithMockUser
 
 public class HomeControllerTest {
 
