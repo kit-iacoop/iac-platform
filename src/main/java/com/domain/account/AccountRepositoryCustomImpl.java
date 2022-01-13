64f532e39afc2f346ebdff1a8665b3f960bb78f8
hyunkim <han.kim@kakao.com>
2022-01-13 18:06:47 +0900

    [chore] customRepository 재정의된 save()에서 persist()후 객체 반환하도록 수정

--- src/main/java/com/domain/account/AccountRepositoryCustomImpl.java
+++ src/main/java/com/domain/account/AccountRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.domain.account;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
@@ -8,19 +9,30 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+@Slf4j
 @Repository
 @Transactional
 public class AccountRepositoryCustomImpl implements AccountRepositoryCustom {
 
     @Autowired
     private EntityManager em;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public void encryptedSave(Account account){
+    public Account encryptedSave(Account account) {
         account.changePassword(passwordEncoder.encode(account.getPassword()));
-        em.persist(account);
+
+        if(account.getAccountId() == null){
+            em.persist(account);
+
+        } else{
+            log.warn("encryptedSave()로는 UPDATE가 불가능합니다.");
+
+        }
+
+        return account;
     }
 
 }
