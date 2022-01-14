80a87eed91cb562866bcae242f564f2ba4c4d064
hyunkim <han.kim@kakao.com>
2022-01-14 10:40:14 +0900

    [chore] 회원가입 성공 페이지 적용, validation 예외 로그 기록

--- src/main/java/com/web/controller/account/AccountController.java
+++ src/main/java/com/web/controller/account/AccountController.java
@@ -1,26 +1,34 @@
 package com.web.controller.account;
 
 
+import com.common.Common;
 import com.domain.account.Company;
 import com.domain.common.State;
 import com.web.dto.CompanyRegisterDTO;
 import com.web.service.AccountService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@AllArgsConstructor
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
+@AllArgsConstructor
+@Slf4j
 @Controller
 public class AccountController {
 
     private final AccountService accountService;
     private final PasswordEncoder passwordEncoder;
+    private final Common common;
 
     @GetMapping( "/register")
     public String register() {
@@ -36,8 +44,13 @@ public class AccountController {
     public ModelAndView createCompany(@Validated @ModelAttribute CompanyRegisterDTO companyDTO, Errors errors, ModelAndView mav){
 
         if(errors.hasErrors()) {
+
+            LinkedList<LinkedHashMap<String, String>> errorList = common.refineErrors(errors);
+            log.warn("COMPANY 회원가입 예외 : " + errorList.toString()); //FE 필터 거치지 않았으므로 WARN으로 기록
+
             mav.setViewName("/register/company");
-            mav.addObject("errors", common.refineErrors(errors));
+            mav.addObject("errors", errorList);
+
             return mav;
         }
 
@@ -45,9 +58,7 @@ public class AccountController {
         Company companyEntity = companyDTO.toEntity();
         companyEntity.verification("BRONZE", 0L, 0L, State.NORMAL); //TODO : 가입 승인 페이지 생길 때 까지..
         accountService.createAccount(companyEntity);
-
-        mav.setViewName("redirect:/");
-
+        mav.setViewName("register/success");
 
         return mav;
     }
