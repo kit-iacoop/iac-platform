638c4c0c340eb63d1eea1a5010621f65de400010
lichee55 <lichee55@gmail.com>
2022-01-15 16:01:46 +0900

    [feature] copyright detail 작업시작

--- src/main/java/com/web/controller/copyright/CopyrightController.java
+++ src/main/java/com/web/controller/copyright/CopyrightController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,5 +43,12 @@ public class CopyrightController {
         models.put("maxPage", dtoList.getTotalPages());
         models.put("key", key);
         return new ModelAndView("copyright/copyright-list").addAllObjects(models);
+    }
+
+    @GetMapping("/copyright/{id}")
+    public ModelAndView viewCopyrightDetail(@PathVariable String id) {
+        
+        Map<String, Object> models = new HashMap<>();
+        return new ModelAndView("copyright/copyright-detail").addAllObjects(models);
     }
 }
