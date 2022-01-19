f0ea7ed4aa91206e9fdc349a54b1ed04f7e1b435
hyunkim <han.kim@kakao.com>
2022-01-19 18:59:10 +0900

    [refactor] Account DTO 상속 적용

---
+++ src/main/java/com/web/dto/account/AccountInformationDTO.java
@@ -0,0 +1,61 @@
+package com.web.dto.account;
+
+import com.domain.account.Account;
+import com.domain.account.Company;
+import com.domain.common.State;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+@SuperBuilder
+public abstract class AccountInformationDTO {
+
+    protected Long accountId;
+
+    @Size(min=4, max=20)
+    @NotBlank
+    protected String loginId;
+
+    @Size(min=8, max=20)
+    @NotBlank
+    protected String password;
+
+    @Size(min=1, max=50)
+    @NotEmpty
+    protected String name;
+
+    protected String birthDate;
+
+    @Size(min=5, max=5)
+    @NotBlank
+    protected String zipCode;
+
+    @Size(min=2, max=30)
+    @NotEmpty
+    protected String city;
+
+    @Size(min=2, max=200)
+    protected String street;
+
+    @Size(min=5, max=200)
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
+    protected String email;
+
+    @Size(min=8, max=15)
+    @NotBlank
+    protected String telephone;
+
+    protected State status;
+
+    abstract public Account toEntity();
+}
--- src/main/java/com/web/dto/account/CompanyInformationDTO.java
+++ src/main/java/com/web/dto/account/CompanyInformationDTO.java
@@ -1,63 +1,27 @@
-package com.web.dto;
+package com.web.dto.account;
 
 import com.domain.account.Company;
 import com.domain.common.Address;
 import com.domain.common.State;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
-@ToString
+@ToString(callSuper=true)
+@SuperBuilder
+@NoArgsConstructor
 
-public class CompanyInformationDTO {
-
-
-    @Size(min=4, max=20)
-    @NotBlank
-    private String loginId;
-
-    @Size(min=8, max=20)
-    @NotBlank
-    private String password;
-
-    @Size(min=1, max=50)
-    @NotEmpty
-    private String name;
+public class CompanyInformationDTO extends AccountInformationDTO {
 
     @Size(min=1, max=50)
     @Positive
     @NotBlank
     private String businessRegistrationNumber;
-
-    private String birthDate;
-
-    @Size(min=5, max=5)
-    @NotBlank
-    private String zipCode;
-
-    @Size(min=2, max=30)
-    @NotEmpty
-    public String city;
-
-    @Size(min=2, max=200)
-    public String street;
-
-    @Size(min=5, max=200)
-    @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
-    private String email;
-
-    @Size(min=8, max=15)
-    @NotBlank
-    private String telephone;
 
     @Size(min=1, max=7)
     @Positive
@@ -80,19 +44,21 @@ public class CompanyInformationDTO {
 
     public Company toEntity(){
         return Company.builder()
+                //공통정보
                 .loginId(loginId)
                 .password(password)
                 .name(name)
-                .businessRegistrationNumber(Long.parseLong(businessRegistrationNumber))
                 .birthDate(LocalDate.parse(birthDate, DateTimeFormatter.ISO_DATE))
                 .address(new Address(city, street, Long.parseLong(zipCode)))
                 .email(email)
                 .telephone(telephone)
+                .status(State.PENDING)
+                //회사정보
+                .businessRegistrationNumber(Long.parseLong(businessRegistrationNumber))
                 .companyType(companyType)
                 .sector(sector)
                 .owner(owner)
                 .subscriptionDate(LocalDate.parse(subscriptionDate, DateTimeFormatter.ISO_DATE))
-                .status(State.PENDING)
                 .employeeNumber(Long.parseLong(employeeNumber))
                 .build()
                 ;
