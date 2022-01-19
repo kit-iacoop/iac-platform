18a11ad375fb22f4afa51cc35da4692113e5be49
hyunkim <han.kim@kakao.com>
2022-01-19 17:34:29 +0900

    [chore] 각자 ROLE 메인화면 접근 매소드들 HomeController로 이주

--- src/main/java/com/web/controller/HomeController.java
+++ src/main/java/com/web/controller/HomeController.java
@@ -7,8 +7,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping(value="/")
-    public String home() throws Exception {
+    public String home() {
         return "home";
     }
+
+    @GetMapping( "/register")
+    public String register() {
+        return "register/main";
+    }
+
+    @GetMapping("/company")
+    public String companyHome() {
+        return "company/main";
+    }
+
+    @GetMapping("/professor")
+    public String professorHome() {
+        return "professor/main";
+    }
+
+    @GetMapping("/officer")
+    public String officerHome() {
+        return "officer/main";
+    }
+
 
 }
