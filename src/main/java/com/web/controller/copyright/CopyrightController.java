b827f9bcdcacc45a950d22e455f5c66fcceb08a3
gh1224 <rlarkgus0@naver.com>
2022-01-19 19:51:20 +0900

    [chore] 페이지 번호 +1

--- src/main/java/com/web/controller/copyright/CopyrightController.java
+++ src/main/java/com/web/controller/copyright/CopyrightController.java
@@ -43,7 +43,7 @@ public class CopyrightController {
         }
         Map<String, Object> models = new HashMap<>();
         models.put("copyrightDtos", dtoList);
-        models.put("page", page.getPageNumber());
+        models.put("page", page.getPageNumber() + 1);
         models.put("maxPage", dtoList.getTotalPages());
         models.put("key", key);
         model.addAllAttributes(models);
