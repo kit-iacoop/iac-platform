9d24afabc0e2db993e9d158814c4302beb3a3255
lichee55 <lichee55@gmail.com>
2022-01-20 15:13:12 +0900

    [feature] collabo-request 관련 기능

--- src/main/java/com/web/controller/request/RequestController.java
+++ src/main/java/com/web/controller/request/RequestController.java
@@ -1,12 +1,20 @@
 package com.web.controller.request;
 
 
+import com.web.service.RequestService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@AllArgsConstructor
+@RequestMapping("requests")
 @Controller
 public class RequestController {
+    private final RequestService requestService;
+
+    public RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+    
 
 }
---
+++ src/main/java/com/web/service/Impl/RequestServiceImpl.java
@@ -0,0 +1,8 @@
+package com.web.service.Impl;
+
+import com.web.service.RequestService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestServiceImpl implements RequestService {
+}
