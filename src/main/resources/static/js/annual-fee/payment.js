92024c4be91060122a935d3f8a8d36a087d366cb
박유경 <51533341+codename-602@users.noreply.github.com>
2022-01-21 14:08:34 +0900

    [feat] 연회비 관련 페이지

---
+++ src/main/resources/static/js/annual-fee/payment.js
@@ -0,0 +1,20 @@
+function changePrice() {
+    var priceElement = document.getElementById("annual-fee-for-grade");
+    var price = priceElement.options[priceElement.selectedIndex].value;
+    var originalPrice = document.getElementsByClassName("original-price")[0];
+    var salePrice = document.getElementsByClassName("sale-price")[0];
+    originalPrice.innerHTML="<p id=\"original-price\">"+ price + "<p>원";
+    salePrice.innerHTML="<p id=\"sale-price\">"+ price + "<p>원";
+}
+
+function allUseDiscount() {
+    var myPoint = document.getElementById("my-point").textContent;
+    var salePrice = document.getElementById("sale-price");
+    salePrice.innerHTML= salePrice.textContent - myPoint;
+}
+
+function discount() {
+    var usingPoint = document.getElementById("use-point").value;
+    var salePrice = document.getElementById("sale-price");
+    salePrice.innerHTML= salePrice.textContent - usingPoint;
+}
