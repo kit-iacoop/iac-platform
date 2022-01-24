6dc99267e6a476cb950250834afee73fdf98a682
hyunkim <han.kim@kakao.com>
2022-01-24 17:57:05 +0900

    [chore] 마일리지 오타 수정

--- src/main/java/com/domain/mileageRequest/MileageRequestRepository.java
+++ src/main/java/com/domain/mileageRequest/MileageRequestRepository.java
@@ -2,6 +2,6 @@ package com.domain.mileageRequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MilageRequestRepository extends JpaRepository<MileageRequest, Long> {
+public interface MileageRequestRepository extends JpaRepository<MileageRequest, Long> {
 
 }
