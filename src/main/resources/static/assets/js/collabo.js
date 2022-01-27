2c4e55a4dea337f6a8b5fff2cbb16fd80275ce20
gh1224 <rlarkgus0@naver.com>
2022-01-27 20:13:20 +0900

    [add] 애로기술 목록 조회 필터 동적 페이지

--- src/main/resources/static/assets/js/collabo.js
+++ src/main/resources/static/assets/js/collabo.js
@@ -1,8 +1,7 @@
-//검색조건 input에 이벤트 넣기
-// const SearchFilterState = {
-//   projectType: "t1", //기본적으로 장기 프로젝트가 선택되어있다고 가정
-//   major: [],
-// };
+movePage = function (page) {
+  $("input[name='page']").val(page);
+  requestProjectList();
+}
 
 getDepth = function (elem) {
   return elem.attr('class').slice(-1) * 1;
@@ -21,44 +20,32 @@ cascadingCheck = function () {
 
 getValues = function () {
   let searchFilterState = {};
-  searchFilterState.projectType = $("input[name='projectType']:checked").val();
+
+  searchFilterState.page = $("input[name='page']").val();
+  searchFilterState.term = $("input[name='term']:checked").val();
+  searchFilterState.type = $("input[name='type']:checked").val();
+
+  searchFilterState.options = [];
+  $.each($("input[name='options']:checked"), function () {
+    searchFilterState.options.push($(this).val());
+  });
+  
   searchFilterState.fields = [];
   $.each($("input[name='fieldCategory']:checked"), function () {
     searchFilterState.fields.push($(this).val());
   });
-  return searchFilterState;
 
-  // var eleType = target.attr("type");
-  // var value = target.attr("data-type");
-  // if (eleType === "radio") {
-  //   SerachFilterState.projectType = value;
-  // }
-  // if (eleType === "checkbox") {
-  //   var index = SerachFilterState.major.indexOf(value);
-  //   //이미 있다면 이것은 uncheck하겠다는 의미
-  //   if (index !== -1) {
-  //     SerachFilterState.major.splice(index, 1);
-  //   } else {
-  //     //없다면 이것은 새로 check되었다는 의미
-  //     SerachFilterState.major.push(value);
-  //     SerachFilterState.major.sort();
-  //   }
-  // }
+  return searchFilterState;
 };
 
 requestProjectList = function () {
-  const searchFilterState = getValues();
-
-  // console.log(searchFilterState);
-  console.log(JSON.stringify(searchFilterState));
-
   $.ajax({
-    // contentType: "application/json",
     method: "GET",
-    url: "/requests/list/filter",
-    data: searchFilterState,
+    url: "/requests/list/query",
+    data: getValues(),
     success: function (fragment) {
-      $('.project-list-container').replaceWith(fragment);
+      $('.project-list-container').html(fragment);
+      $("html, body").animate({ scrollTop: 0 }, "fast");
     },
     error: function (errorThrown) {
       alert(errorThrown.statusText);
@@ -71,7 +58,9 @@ $(function () {
     change: cascadingCheck,
   });
 
-  $(".sidebar-body input").on({
-    change: requestProjectList,
+  $("#filterBtn").on({
+    click: requestProjectList,
   });
+
+  movePage(1);
 });
