25cf875a51e4b45c0f3bb7688f64323a5985ea4e
hyunkim <han.kim@kakao.com>
2022-01-27 10:44:22 +0900

    [chore] 머지 과정 하 import 누락 부분 다시 추가

--- src/main/java/com/web/controller/account/AccountController.java
+++ src/main/java/com/web/controller/account/AccountController.java
@@ -20,9 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
