db83b5e353e06ef2bff18e272fa6cdb547c75494
gh1224 <rlarkgus0@naver.com>
2022-02-04 00:04:14 +0900

    [chore] @Param

--- src/main/java/com/domain/noticeBoard/NoticeBoardRepository.java
+++ src/main/java/com/domain/noticeBoard/NoticeBoardRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Long> {
     Page<NoticeBoard> findAllByNoticeBoardTitleContains(Pageable pageable, String key);
 
     @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId > :id order by b.noticeBoardId asc")
-    Page<NoticeBoardDTO> findNextBoardId(Long id, Pageable pageable);
+    Page<NoticeBoardDTO> findNextBoardId(@Param("id") Long id, Pageable pageable);
 
     @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId < :id order by b.noticeBoardId desc")
-    Page<NoticeBoardDTO> findPrevBoardId(Long id, Pageable pageable);
+    Page<NoticeBoardDTO> findPrevBoardId(@Param("id") Long id, Pageable pageable);
 }
