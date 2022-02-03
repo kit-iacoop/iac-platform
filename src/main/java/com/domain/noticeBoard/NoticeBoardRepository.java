d9814895285eec7ed54a3aff69825e0b4d082443
lichee55 <lichee55@gmail.com>
2022-02-04 00:00:31 +0900

    [fix] prevBoard, nextBoard 기능 정렬

--- src/main/java/com/domain/noticeBoard/NoticeBoardRepository.java
+++ src/main/java/com/domain/noticeBoard/NoticeBoardRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Long> {
     Page<NoticeBoard> findAllByNoticeBoardTitleContains(Pageable pageable, String key);
 
-    @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId > :id order by b.noticeBoardId desc")
+    @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId > :id order by b.noticeBoardId asc")
     Page<NoticeBoardDTO> findNextBoardId(Long id, Pageable pageable);
 
-    @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId < :id order by b.noticeBoardId asc")
+    @Query("select new com.web.dto.NoticeBoardDTO(b.noticeBoardId,b.noticeBoardTitle) from NoticeBoard b where b.noticeBoardId < :id order by b.noticeBoardId desc")
     Page<NoticeBoardDTO> findPrevBoardId(Long id, Pageable pageable);
 }
